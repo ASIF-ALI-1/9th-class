e42acd434d7db879845f5d9f1a3e21f79894c68e
Asif-Ali-1 <asif7171786@gmail.com>
2024-07-17 22:29:40 +0500

    init class

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css"
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="main">
+      <div class="contianer">
+          <div class="logo">
+          <img src="assits\new.png"></img>
+          <li>Asif Ali</li>
+           </div>
+        <div class= "navbar">
+
+          <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Dashbord</a></li>
+          <li><a href="#">Saved</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Setting</a></li>
+          
+          </ul>
+        </div>
+
+
+
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
